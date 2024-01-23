490b3d76a419f2e446ba262a9f97308b50780b90
DFimman <dichosaimmanuel@gmail.com>
2024-01-23 15:36:10 +0800

    Added increase, decrease, reset

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,31 @@ import "./App.css";
 import { useState } from "react"; //to create a state
 
 function App() {
-const [count, setCount] = useState(0);
+  const [count, Setcount] = useState(0);
 
-return (
-<div className="App">
-       <button> Increase </button>
-       <button> Decrease </button>
-       <button> Set to Zero </button>
-       <h1> {count} </h1>
-</div>
-);
+  const increase = () => {
+    Setcount((prevState) => count + 1);
+  };
+
+  const decrease = () => {
+    Setcount((prevState) => (prevState > 0 ? prevState - 1 : prevState));
+  };
+
+  const reset = () => {
+    Setcount(0);
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center text-center vh-100">
+      <div>
+        <h1>{count}</h1>
+        <button> Increase </button>
+        <button> Decrease </button>
+        <button> Set to Zero </button>
+        <h1> {count} </h1>
+      </div>
+    </div>
+  );
 }
+
+export default App;
